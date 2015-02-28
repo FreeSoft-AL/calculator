750b2f2ab6e0089720349345d14bee8841a0424a
Dashamir Hoxha <dashohoxha@gmail.com>
2015-02-28 09:32:57 +0100

    Kur klikohet një numër, shfaqet te fusha e llogaritjeve (fixes #7).

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 
+$(document).ready(function () {
+
+    $('.calc .ui-btn').on('click', function () {
+        var nr = $(this).html();
+        console.log(nr);
+        $('#display').attr('value', nr);
+    });
+
+});
